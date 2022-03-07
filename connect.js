98162eb603ad84c10dc33eb111a1a208d4e0835e
A <chav3z.eth@gmail.com>
2022-03-07 15:29:35 -0500

    initial commit

---
+++ connect.js
@@ -0,0 +1,19 @@
+async function loadWeb3() {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+    }
+}
+
+async function load() {
+    await loadWeb3();
+    updateStatus('Ready!');
+}
+
+function updateStatus(status) {
+    const statusEl = document.getElementById('status');
+    statusEl.innerHTML = status;
+    console.log(status);
+}
+
+load();
